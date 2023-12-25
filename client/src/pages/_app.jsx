cb9ddf9a0dbdfae6df93bdbaa2b942593096be69
Kushagra-Gupta01 <kushagra.gupta0077@gmail.com>
2023-12-25 13:51:34 +0530

    2nd commit

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -1,5 +1,15 @@
+import { StateProvider } from "@/context/StateContext";
+import reducer, { initialState } from "@/context/StateReducers";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return(
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <Head>
+        <title>WhatsApp</title>
+        <link rel="Shortcut Icon" href="/favicon.png"></link>
+      </Head>
+    </StateProvider>
+  )
 }
--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import Image from "next/image";
+import { useStateProvider } from "@/context/StateContext";
 function onboarding() {
-  return <div>onboarding</div>;
+  const [{ userInfo }] = useStateProvider
+  console.log({userInfo})
+  return (
+  <div className="flex items-center justify-center flex-col bg-panel-header-background h-screen w-screen text-white">
+    <div className="flex items-center justify-center gap-2">
+      <Image src="/whatsapp.gif" alt="WhatsApp" height={300} width={300}/>
+      <span className="text-7xl">WhatsApp</span>
+    </div>
+    <h2 className="text-2xl">Create Your Profile</h2>
+    <div className="flex gap-6 mt-6">
+      <div className="flex flex-col items-center justify-center mt-5 gap-6">
+
+      </div>
+    </div>
+  </div>
+  )
 }
 
 export default onboarding;
